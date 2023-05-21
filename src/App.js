d184a9db20bf14c0a79a8eea2ad89f79e13782ff
Victor Henrique Fernandes <victorhenriquefernandes2@gmail.com>
2023-05-21 01:12:45 -0300

    requisito3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   state = {
@@ -10,17 +11,9 @@ class App extends React.Component {
     cardAttr3: '',
     cardImage: '',
     cardRare: '',
-    cardTrunfo: false,
+    cardTrunfo: '',
     hasTrunfo: false,
     isSaveButtonDisable: false,
-  };
-
-  onInputChange = () => {
-
-  };
-
-  onSaveButtonClick = () => {
-
   };
 
   render() {
@@ -42,6 +35,16 @@ class App extends React.Component {
           hasTrunfo={ hasTrunfo }
           isSaveButtonDisable={ isSaveButtonDisable }
 
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
         />
       </div>
     );
